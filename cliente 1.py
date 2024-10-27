5abb83793fce7b32062e3085f4af30b7a74b1543
Gabriel Victor <erivaldo.gabriel9@gmail.com>
2024-10-26 22:45:47 -0300

    erro ao digitar sair corrigido//readme atualizado.

--- cliente 1.py
+++ cliente 1.py
@@ -26,16 +26,23 @@ def descriptografar(iv, ct, key):
 # Função para receber mensagens da fila
 def receber_mensagens(canal, key):
     while True:
-        method_frame, header_frame, body = canal.basic_get(queue='Cliente2', auto_ack=True)
-        if method_frame:  
-            print(f"Mensagem criptografada recebida: {body.decode('utf-8')}")
-            iv, ct = body.decode('utf-8').split(':')  
-            try:
-                mensagem_decryptada = descriptografar(iv, ct, key)
-                print(f"\nUsuário 2: {mensagem_decryptada}") 
-            except Exception as e:
-                print(f"Erro ao descriptografar a mensagem: {e}")  
-        time.sleep(0.5)  
+        try:
+            method_frame, header_frame, body = canal.basic_get(queue='Cliente2', auto_ack=True)
+            if method_frame:  
+                print(fg('yellow') + f"Mensagem criptografada recebida: {body.decode('utf-8')}" + attr('reset'))
+                iv, ct = body.decode('utf-8').split(':')  
+                try:
+                    mensagem_decryptada = descriptografar(iv, ct, key)
+                    print(f"\nUsuário 2: {mensagem_decryptada}") 
+                except Exception as e:
+                    print(f"Erro ao descriptografar a mensagem: {e}")  
+        except rabbit.exceptions.StreamLostError:
+            print(fg('red') + "Conexão com o servidor perdida." + attr('reset'))
+            break
+        except Exception as e:
+            print(f"Erro ao receber mensagem: {e}")
+            break
+        time.sleep(0.5)
 
 conexao = rabbit.BlockingConnection(rabbit.ConnectionParameters(
     host='15.228.248.2',
